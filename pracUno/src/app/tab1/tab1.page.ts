eef8e4ca7ea6db79046d866a396921e8d2ff17a5
zabdielcead <zabdiel.cead@gmail.com>
2019-05-28 17:51:30 -0500

    ionic debugger func completa

--- pracUno/src/app/tab1/tab1.page.ts
+++ pracUno/src/app/tab1/tab1.page.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
-
+export class Tab1Page  implements OnInit {
+  /*
+    ionic 4 implementa angular ciclos de vida
+    debugger es un breakpoint
+  */
   constructor() {}
+
+  ngOnInit() {
+    let numero = 10;
+    // tslint:disable-next-line:no-debugger
+    debugger;
+    numero += 10;
+    numero *= 2;
+    console.log({ numero }); // aparecera en consola el numero y la varible
+  }
 
 }
